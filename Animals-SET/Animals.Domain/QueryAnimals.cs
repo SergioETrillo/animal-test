b5a33669913cc28c6b3eec06717eb852ef09d68a
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-08-16 23:53:13 +0100

    add LINQ queries

---
+++ Animals-SET/Animals.Domain/QueryAnimals.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Animals_SET.Animals.Domain
+{
+    public static class QueryAnimals
+    {
+        public static List<Animal> GetAnimalsByName(string name)
+        {
+            var animalsByName = GroupOfAnimals.Members().Where(p => p.Name == name).ToList();
+
+            return animalsByName;
+        }
+
+        public static List<Animal> GetAnimalsOlderThan(int age)
+        {
+            var animalsByName = GroupOfAnimals.Members().Where(p => p.Age > age).ToList();
+
+            return animalsByName;
+        }
+
+        public static List<Animal> SortAnimalsByNameThenAge()
+        {
+            var sortedAnimals = GroupOfAnimals.Members().OrderBy(p => p.Name)
+                                                        .ThenBy(p => p.Age).ToList();
+
+            return sortedAnimals;
+        }
+    }
+}
