6b118fae3a18b3eaf0093a246a83f07973cdbb46
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-08-17 01:06:18 +0100

    add serialize the list to JSON

--- Animals-SET/Animals.Domain/GroupOfAnimals.cs
+++ Animals-SET/Animals.Domain/GroupOfAnimals.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,6 +19,11 @@ namespace Animals_SET.Animals.Domain
             };
 
             return listOfAnimals;
+        }
+
+        public static string serializeList()
+        {
+            return JsonConvert.SerializeObject(Members());
         }
     }
 }
