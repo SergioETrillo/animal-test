ac9c1aa016f75da0f90c269a5b0782d7e0b3dc71
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-08-17 15:03:27 +0100

    add tests for LINQ queries

---
+++ Animals-SETTests/Animals.Domain/QueryAnimalsTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Animals_SET.Animals.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals_SET.Animals.Domain.Tests
+{
+    [TestClass()]
+    public class QueryAnimalsTests
+    {
+        [TestMethod()]
+        public void GetAnimalsByNameTest()
+        {
+            //Arrange
+            var filtered = QueryAnimals.GetAnimalsByName("Tweety");
+            //Act
+            var expectedCount = 1;
+            var expectedId = 3;
+
+
+            //Assert
+            var actualCount = filtered.Count;
+            var actualId = filtered[0].Id;
+
+            Assert.AreEqual(expectedCount, actualCount);
+            Assert.AreEqual(expectedId, actualId);
+        }
+
+        [TestMethod()]
+        public void GetAnimalsOlderThanTest()
+        {
+            //Arrange
+            var filtered = QueryAnimals.GetAnimalsOlderThan(2);
+            //Act
+            var expectedCount = 2;
+            var expectedFirstId = 1;
+            var expectedLastId = 2;
+
+
+            //Assert
+            var actualCount = filtered.Count;
+            var actualFirstId = filtered[0].Id;
+            var actualLastId = filtered[1].Id;
+
+            Assert.AreEqual(expectedCount, actualCount);
+            Assert.AreEqual(expectedFirstId, actualFirstId);
+            Assert.AreEqual(expectedLastId, actualLastId);
+        }
+    }
+}
