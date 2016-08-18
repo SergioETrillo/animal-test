4a01da0542b8692128824e8c649a17c200281677
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-08-18 22:22:30 +0100

    add create action in Animals controller

--- Animals-SET/Controllers/AnimalsController.cs
+++ Animals-SET/Controllers/AnimalsController.cs
@@ -36,16 +36,33 @@ namespace Animals_SET.Controllers
         [HttpPost]
         public ActionResult Create(FormCollection collection)
         {
-            try
+            if (ModelState.IsValid)
             {
-                // TODO: Add insert logic here
+                string name = Request.Form["Name"].ToLower();
+                string type = Request.Form["Type"].ToLower();
+                int age = Int32.Parse(Request.Form["Age"]);
+                char gender = Convert.ToChar(Request.Form["Gender"].ToUpper());
+                int id = Zoo.Members.Count + 1;  // To create a unique ID
+                Animal animal;
+                switch (type)
+                {
+                    case "bird":
+                        animal = new Bird(name, age, gender);
+                        break;
+                    case "cat":
+                        animal = new Cat(name, age, gender);
+                        break;
+                    case "dog":
+                        animal = new Dog(name, age, gender);
+                        break;
+                    default:
+                        return RedirectToAction("Index"); // Error handling missing
+                }
+                Zoo.Members.Add(animal);
 
                 return RedirectToAction("Index");
             }
-            catch
-            {
-                return View();
-            }
+            return View();
         }
 
         // GET: Animals/Edit/5
@@ -92,11 +109,11 @@ namespace Animals_SET.Controllers
             }
         }
 
-        static List<Animal> _zoo = new List<Animal>()
-            {
-                new Dog { Id = 1, Name = "Fluffy-group", Age = 33, Gender = 'M' },
-                new Cat { Id = 2, Name = "Cattie-group", Age = 44, Gender = 'F'},
-                new Bird {Id = 3, Name = "Tweety-group", Age = 11, Gender = 'M' }
-            };
+        //static List<Animal> _zoo = new List<Animal>()
+        //    {
+        //        new Dog { Id = 1, Name = "Fluffy-group", Age = 33, Gender = 'M' },
+        //        new Cat { Id = 2, Name = "Cattie-group", Age = 44, Gender = 'F'},
+        //        new Bird {Id = 3, Name = "Tweety-group", Age = 11, Gender = 'M' }
+        //    };
     }
 }
